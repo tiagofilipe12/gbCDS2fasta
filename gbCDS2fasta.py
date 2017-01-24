f77c6817bd8282035e29b970e87d22f35d91f723
Tiago Jesus <tiagofilipe12@gmail.com>
2017-01-24 11:23:47 +0000

    Added several functionalities to make fasta for each CDS, parsing the output from FeaturesLocation from biopython. Still, There is no implementation of reverse complement

--- gbCDS2fasta.py
+++ gbCDS2fasta.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
-## Last update: 23/1/2017
+## Last update: 24/1/2017
 ## Author: T.F. Jesus
 ## This script retrieves CDS data from .gb files to a fasta in which each CDS is a sequence entry
 ## Untested script and lacks some functions
 
 import argparse
+import os
 from Bio import SeqIO
 
 def header_fix(input_header):
@@ -15,34 +16,49 @@ def header_fix(input_header):
 	return input_header
 
 def genbank_file_reader(input_dir, output_file):	
+	out_handle=open(output_file,'w')
 	for infile in os.listdir(input_dir):
-		if input_dir.endswith(".gb"):
+		if infile.endswith(".gb"):
 			cds_gene_position = {}	
-			record = SeqIO.read(infile, "genbank")
-			gb_description=gbkdata.description[:-1]
+			record = SeqIO.read(os.path.join(input_dir,	infile), "genbank")
+			gb_description=record.description[:-1]
 			seq_description=header_fix(gb_description)
-			sequence = str(gbkdata.seq)			
+			sequence = str(record.seq)			
 			seq_id = record.id
 			tag_name = "ref_" + seq_id + "_" + seq_description + "_"  
 			for feat in record.features:
-				strand = feat.location.strand
+#				strand = feat.location.strand
 				seq_type = feat.type
 				if seq_type == "CDS":
-					location = feat.location ## did not use .start and .end because it does not retrieve properly when DNA sequence is circular
-					if "gene" in feat.qualifiers.keys():
-						tag_name =+ "CDS_" + feat.qualifiers.values()[0]
-						cds_gene_position[tag_name] = location
-			cds2fasta(output_file, cds_gene_position, sequence)	
+					cds_tag_name = ""
+					location = str(feat.location) ## did not use .start and .end because it does not retrieve properly when DNA sequence is circular
+					for key in feat.qualifiers.keys():
+						cds_tag_name = tag_name + "CDS_" + location
+					cds_gene_position[cds_tag_name] = location
+					print cds_tag_name
+			cds2fasta(out_handle, cds_gene_position, sequence)	
 
-def cds2fasta(output_file, cds_gene_position, sequence):
-	out_handle=open(output_file,'w')
+def cds2fasta(out_handle, cds_gene_position, sequence):
+	counter_start = 0
+	counter_end = 0
 	for k in cds_gene_position.keys():
-		out_handle.write(">" + cds_gene_position.keys() + "\n")
-		out_handle.write(sequence)
+		print k		
+		start_location = cds_gene_position[k].split(":")[0].lstrip("[")
+		end_location = cds_gene_position[k].split(":")[1].split("]")[0]
+		complement = cds_gene_position[k].split(":")[1].split("]")[1] ## (+) or (-)
+		if ">" or "<" in start_location:
+			counter_start =+1
+			start_location = start_location.lstrip("<").lstrip(">")
+		if ">" or "<" in end_location:
+			counter_end =+ 1
+			end_location = end_location.lstrip("<").lstrip(">")
+		out_handle.write(">" + k + "\n")
+		out_handle.write(sequence[int(start_location):int(end_location)] + "\n")
 
 def main():
-	parser.add_argument('-in','--input', dest='inputfile', required=True, help='Provide the input fasta files to parse')
-	parser.add_argument('-out','--output', dest='outputfile', required=True, help='Provide the output directory')
+	parser = argparse.ArgumentParser(description="Converts all CDS in a .gb file into a fasta with each CDS region as an individual sequence")
+	parser.add_argument('-in','--input', dest='inputfile', required=True, help='Provide the input directory with .gb (GenBank) files to parse')
+	parser.add_argument('-out','--output', dest='outputfile', required=True, help='Provide the output file name')
 	args = parser.parse_args()
 	list_of_gb = args.inputfile.split(" ")
 	for gb in list_of_gb:
